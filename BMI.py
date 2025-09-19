c99189f7430df0125b0f98a9c83c80cd4b78f911
UmairDevelop <accforumair@gmail.com>
2025-09-19 21:06:37 +0500

    Moved

--- BMI.py
+++
@@ -1,8 +0,0 @@
-name = input("Enter name:\n")
-age = input("Enter age:\n")
-height = input("Enter height in feet:\n")
-weight = input("Enter weight in kg:\n")
-
-height_meters = float(height) * 0.3048 # Convert height to meters
-bmi = float(weight) / (height_meters ** 2) # BMI calculation
-print(f"{name}, your BMI is {bmi:.2f}")
